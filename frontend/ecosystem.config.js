bba2f98018097ce29e5a67e507b919200611e7d2
Maria Kuleshova <m1800597@edu.misis.ru>
2025-01-11 17:23:54 +0300

    Деплой фронтенда

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -5,12 +5,6 @@ const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF } =
   process.env;
 
 module.exports = {
-  apps: [
-    {
-      name: "mesto",
-      script: "dist/app.js",
-    },
-  ],
   deploy: {
     production: {
       user: DEPLOY_USER,
@@ -18,8 +12,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      "post-deploy": "cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+      "post-deploy": "cd frontend && pwd && npm ci && npm run build",
     },
   },
 };
